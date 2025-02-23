461f6413b1ab2fd513be30a9b5e6e38960fb1cf3
Thomas Carr <carrt313@gmail.com>
2025-02-23 18:03:38 -0500

    ntu120

--- NTU/SGN/seq_transformation.py
+++ NTU/SGN/seq_transformation.py
@@ -206,41 +206,39 @@ def get_indices(performer, camera, evaluation='CS'):
     train_indices = np.empty(0)
 
     if evaluation == 'CS':  # Cross Subject (Subject IDs)
-        train_ids = [1,  2,  4,  5,  8,  9,  13, 14, 15, 16,
-                     17, 18, 19, 25, 27, 28, 31, 34, 35, 38]
-        test_ids = [3,  6,  7,  10, 11, 12, 20, 21, 22, 23,
-                    24, 26, 29, 30, 32, 33, 36, 37, 39, 40]
+        train_ids = [1, 2, 4, 5, 8, 9, 13, 14, 15, 16, 17, 18, 19, 25, 27, 28, 31, 34, 35, 38, 45, 46, 47, 49, 50, 52, 53, 54, 55, 56, 57, 58, 59, 70, 74, 78, 80, 81, 82, 83, 84, 85, 86, 89, 91, 92, 93, 94, 95, 97, 98, 100, 103]
+        test_ids = [3,6,7,10,11,12,20,21,22,23,24,26,29,30,32,33,36,37,39,40,41,42,43,44,48,51,60,61,62,63,64,65,66,67,68,69,71,72,73,75,76,77,79,87,88,90,96,99,101,102,104,105,106]
 
         # Get indices of test data
         for idx in test_ids:
             temp = np.where(performer == idx)[0]  # 0-based index
-            test_indices = np.hstack((test_indices, temp)).astype(np.int)
+            test_indices = np.hstack((test_indices, temp)).astype(np.int32)
 
         # Get indices of training data
         for train_id in train_ids:
             temp = np.where(performer == train_id)[0]  # 0-based index
-            train_indices = np.hstack((train_indices, temp)).astype(np.int)
+            train_indices = np.hstack((train_indices, temp)).astype(np.int32)
     else:  # Cross View (Camera IDs)
         train_ids = [2, 3]
         test_ids = 1
         # Get indices of test data
         temp = np.where(camera == test_ids)[0]  # 0-based index
-        test_indices = np.hstack((test_indices, temp)).astype(np.int)
+        test_indices = np.hstack((test_indices, temp)).astype(np.int32)
 
         # Get indices of training data
         for train_id in train_ids:
             temp = np.where(camera == train_id)[0]  # 0-based index
-            train_indices = np.hstack((train_indices, temp)).astype(np.int)
+            train_indices = np.hstack((train_indices, temp)).astype(np.int32)
 
     return train_indices, test_indices
 
 
 if __name__ == '__main__':
-    camera = np.loadtxt(camera_file, dtype=np.int)  # camera id: 1, 2, 3
-    performer = np.loadtxt(performer_file, dtype=np.int)  # subject id: 1~40
-    label = np.loadtxt(label_file, dtype=np.int) - 1  # action label: 0~59
+    camera = np.loadtxt(camera_file, dtype=np.int32)  # camera id: 1, 2, 3
+    performer = np.loadtxt(performer_file, dtype=np.int32)  # subject id: 1~40
+    label = np.loadtxt(label_file, dtype=np.int32) - 1  # action label: 0~59
 
-    frames_cnt = np.loadtxt(frames_file, dtype=np.int)  # frames_cnt
+    frames_cnt = np.loadtxt(frames_file, dtype=np.int32)  # frames_cnt
     skes_name = np.loadtxt(skes_name_file, dtype=np.string_)
 
     with open(raw_skes_joints_pkl, 'rb') as fr:
@@ -250,7 +248,12 @@ if __name__ == '__main__':
 
     skes_joints = align_frames(skes_joints, frames_cnt)  # aligned to the same frame length
 
-    evaluations = ['CS', 'CV']
-    for evaluation in evaluations:
-        # split_dataset(skes_joints, label, performer, camera, evaluation, save_path)
-        split_dataset_dict(skes_joints, skes_name, performer, camera, evaluation, save_path)
+    # evaluations = ['CS', 'CV']
+    # for evaluation in evaluations:
+    #     # split_dataset(skes_joints, label, performer, camera, evaluation, save_path)
+    #     split_dataset_dict(skes_joints, skes_name, performer, camera, evaluation, save_path)
+
+    # Save to pickle where filename is key and value is skeleton
+    X_full = {name: skes_joints[i] for i, name in enumerate(skes_name)}
+    with open(os.path.join(save_path, 'X_full.pkl'), 'wb') as f:
+        pickle.dump(X_full, f)
